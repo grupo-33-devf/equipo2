f6be82acdb8b2db9cb445defd78edc6110c69a32
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-12-07 01:13:49 -0700

    Creacion de componentes sencillos

---
+++ frontend/Pages/Encuestas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Encuestas = () => {
+    return (
+        <div>Encuestas</div>
+    )
+}
+
+export default Encuestas
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Encuestas from '../Pages/Encuestas'
+import Home from '../Pages/Home'
+import Loggin from '../Pages/Loggin'
+import Signup from '../Pages/Signup'
 
 function App() {
 
   return (
     <>
       <h1>Encuestas</h1>
+      <Home />
+      <Encuestas />
+      <Loggin />
+      <Signup />
+
     </>
   )
 }
