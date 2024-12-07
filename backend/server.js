c28806a0633ed26db286db7255ca317296940fa9
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-12-07 03:54:40 -0700

    Implementación de cors

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,34 @@
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
-const express = require('express')
-const connectDB = require('./config/db')
-const { errorHandler } = require('./middleware/errorMiddleware')
-const cors = require('cors')
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const express = require('express');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}))
+}));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.options('*', cors());
 
-
-app.use('/api/users', require('./routes/usuariosRoutes'))
-app.use('/api/encuestas', require('./routes/encuestasRoutes'))
-app.use('/api/preguntas', require('./routes/preguntasRoutes'))
-app.use('/api/respuestas', require('./routes/respuestasRoutes'))
-app.use('/api/urls', require('./routes/urlsRoutes'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
-app.use(errorHandler)
+app.use('/api/users', require('./routes/usuariosRoutes'));
+app.use('/api/encuestas', require('./routes/encuestasRoutes'));
+app.use('/api/preguntas', require('./routes/preguntasRoutes'));
+app.use('/api/respuestas', require('./routes/respuestasRoutes'));
+app.use('/api/urls', require('./routes/urlsRoutes'));
 
-app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`))
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`.yellow.bold));
