43a2786a973035a05a3800690eb935586a97b3e8
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-12-07 00:54:11 -0700

    Instalamos bootstrap y sass

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
