2f4f204d39c76b67aa2c082c434c024540de1c08
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-03-05 19:54:07 -0700

    Cambios menores en el footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-body-secondary">© 2024 Insight Master, Dev</p>
+                <p className="col-md-4 mb-0 text-body-secondary">© 2025 Insight Master, Dev</p>
                 <a
                     href="/"
                     className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
